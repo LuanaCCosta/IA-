1a4d57dfb89fc56c7168702496d1ce2ec968ca07
LuanaCCosta <115171389+LuanaCCosta@users.noreply.github.com>
2023-06-12 08:31:16 -0300

    Update agents.py
    A*, Menor custo, Guloso e profundidade

--- agents.py
+++ agents.py
@@ -29,10 +29,11 @@ class AgentDFS:
 
             if (self.percepcoes['posicao'] == self.percepcoes['saida']).all():
                 ################ VIS ###########
-                self.plotar_caminho(self.ambiente,path,3)
+                plotar_caminho(self.ambiente,path,3)
                 ################################
                 return path
             else:
+                #poda de ciclos
                 for vi in self.percepcoes['vizinhos']:
                     forma_ciclo = False
                     for no in path:
@@ -42,14 +43,15 @@ class AgentDFS:
                     if not forma_ciclo:
                         self.F.append(path + [vi])
         
-class AgentGreedy:
-    def __init__(self, ambiente) -> None:
+class AgentEsperto:
+    def __init__(self, ambiente, type='Profundidade') -> None: #Seleciona o algoritimo 
         self.ambiente = ambiente
         self.percepcoes = ambiente.percepcoes_iniciais()
+        self.type = type
         self.F = [[self.percepcoes['posicao']]]
-        self.C = []
+        self.C = [cost([self.percepcoes['posicao']])] 
         self.H = [heuristica(self.percepcoes['posicao'],self.percepcoes['saida'])]
-        self.C_H = []
+        self.C_H = [self.C[-1]+self.H[-1]] # soma custo e heuristica para obter o A*
 
     def act(self):
 
@@ -58,11 +60,20 @@ class AgentGreedy:
 
         plt.ion()
         while self.F:
-            
-            posicao_min_h_na_fronteira = np.argmin(self.H)
-            
-            path = self.F.pop(posicao_min_h_na_fronteira)
-            self.H.pop(posicao_min_h_na_fronteira) 
+            # Verifica qual agente foi selecionado e seleciona posição na fronteira de acordo com o critério escolhido
+            if self.type == 'Greedy':
+                posicao_na_fronteira = np.argmin(self.H)
+            elif self.type == 'LowestCost':
+                posicao_na_fronteira = np.argmin(self.C)
+            elif self.type == 'AStar':
+                posicao_na_fronteira = np.argmin(self.C_H)
+            elif self.type == 'Profundidade':
+                posicao_na_fronteira = np.argmin(self.F)
+
+            path = self.F.pop(posicao_na_fronteira)
+            self.H.pop(posicao_na_fronteira)
+            self.C.pop(posicao_na_fronteira)
+            self.C_H.pop(posicao_na_fronteira) 
             
             action = {'mover_para':path[-1]}
 
@@ -74,22 +85,26 @@ class AgentGreedy:
 
             if (self.percepcoes['posicao'] == self.percepcoes['saida']).all():
                 ################ VIS ###########
-                self.plotar_caminho(self.ambiente,path,4)
+                plotar_caminho(self.ambiente,path,4)
                 ################################
                 return path
             else:
-                for vi in self.percepcoes['vizinhos']:
+                for vi in self.percepcoes['vizinhos']:  #Verifica se forma ciclos
                     forma_ciclo = False
                     for no in path:
                         if (no == vi).all():
                             forma_ciclo = True
 
-                    if not forma_ciclo:
+                    if not forma_ciclo: #Poda de ciclo
                         self.F.append(path + [vi])
                         self.H.append(heuristica(vi,self.percepcoes['saida']))
+                        self.C.append(cost(path + [vi]))
+                        self.C_H.append(self.H[-1] + self.C[-1])
 
+def cost(path):  #calcula o custo
+    return len(path)-1
 
-def heuristica(no, objetivo):
+def heuristica(no, objetivo): #calcula a heuristica
     manhattan_distance = np.sum(np.abs(no-objetivo))
     return manhattan_distance
 
@@ -103,5 +118,7 @@ def plotar_caminho(ambiente, caminho, tempo):
     plt.pause(tempo)
     plt.clf()
 
+
+           
 
            
